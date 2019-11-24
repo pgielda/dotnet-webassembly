9a7c9530e39037162fc9b4796930b4034909e45e
Peter Gielda <pgielda@antmicro.com>
2019-11-24 23:43:15 +0100

    Naive printf.

--- main.cs
+++ main.cs
@@ -5,21 +5,39 @@ using WebAssembly;
 using WebAssembly.Instructions;
 using WebAssembly.Runtime;
 
+using System.Runtime.InteropServices;
+
 static class Program
 {
-    // exported function
-    public static void sayc(int value)
-    {
-	Console.WriteLine("Saying {0}", (char)value);
+    public static void greet() {
+    	Console.WriteLine("hello world!");
+    }
+
+    public static int printf(int s, int b) {
+    	Console.WriteLine("printf offset={0} values_offset={1}", s, b);
+	string st = Marshal.PtrToStringAuto(memory.Start + s);
+	Console.WriteLine("printf s='{0}'", st);
+
+        for (int i = 0; i < 255; i++) {
+          long val = Marshal.ReadInt32(memory.Start+b+i*4);
+          if (val == 0) break;
+          Console.WriteLine("argument {0}", val);
+        }
+	
+	return 0;
     }
 
     static void Main()
     {
         var imports = new ImportDictionary
         {
-                    { "env", "sayc", new FunctionImport(new Action<int>(sayc)) },
+		    { "env", "greet", new FunctionImport(new Action(greet)) },
+		    { "env", "printf", new FunctionImport(new Func<int, int, int>(printf)) },
         };
         var compiled = Compile.FromBinary<dynamic>(new FileStream("test.wasm", FileMode.Open, FileAccess.Read))(imports);
-        compiled.Exports.test();
+	memory = compiled.Exports.memory;
+        compiled.Exports._start();
     }
+
+    static UnmanagedMemory memory;
 }
--- make.sh
+++ make.sh
@@ -1,3 +1,7 @@
 csc WebAssembly/*.cs WebAssembly/Runtime/*.cs WebAssembly/Runtime/Compilation/*.cs WebAssembly/Instructions/*.cs main.cs -langversion:7.3
-clang --target=wasm32 -nostdlib -Wl,--no-entry  -Wl,--export-all -Wl,--allow-undefined -o test.wasm test.c
+echo "List of undefined:"
+clang --target=wasm32 -nostdlib -Wl,--no-entry  -Wl,--export-all -o test.wasm /root/Desktop/libclang_rt.builtins-wasm32.a test.c 2>&1 | grep undefined | rev | cut -f 1 -d ':' | rev 
+echo
+echo "Compiling."
+clang --target=wasm32 -nostdlib -Wl,--export-all -Wl,--allow-undefined -Wl,--no-entry -o test.wasm test.c
 

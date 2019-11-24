6279ce2ee7879273dfdd715ffd027cfb383276a5
Peter Gielda <pgielda@antmicro.com>
2019-11-24 23:45:19 +0100

    Remove leftover from make.sh

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 csc WebAssembly/*.cs WebAssembly/Runtime/*.cs WebAssembly/Runtime/Compilation/*.cs WebAssembly/Instructions/*.cs main.cs -langversion:7.3
 echo "List of undefined:"
-clang --target=wasm32 -nostdlib -Wl,--no-entry  -Wl,--export-all -o test.wasm /root/Desktop/libclang_rt.builtins-wasm32.a test.c 2>&1 | grep undefined | rev | cut -f 1 -d ':' | rev 
+clang --target=wasm32 -nostdlib -Wl,--no-entry  -Wl,--export-all -o test.wasm test.c 2>&1 | grep undefined | rev | cut -f 1 -d ':' | rev 
 echo
 echo "Compiling."
 clang --target=wasm32 -nostdlib -Wl,--export-all -Wl,--allow-undefined -Wl,--no-entry -o test.wasm test.c
